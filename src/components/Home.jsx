cb07cc1bffd29c961e94b40f517c67c7e4052ce8
manasa y <123yapitsme@gmail.com>
2024-10-03 21:54:18 +0530

    done with the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import '../styles/Home.css'
+import React from "react";
+import img from "../assets/img-1.webp";
+import image from "../assets/img-2.jpeg";
+
+import "../styles/Home.css";
 function Home() {
   return (
-    <div className='home'>
-      home, hello there
+      <div className="container">
+        <div className="content-left">
+          <h1>A cool barter system</h1>
+          <p>Exchange your skills for a new one</p>
+          <button className="cta-button">Let's Start</button>
+        </div>
+        <div className="content-right">
+        
+        <img src={image} alt="hero image" className="image2"/>
+        <img src={img} alt="hero image" className="image1" />
+        </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
