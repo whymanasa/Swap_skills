c14c6d7ca1ac65fc525f9730fb0875a7457c9571
Yadavpreethi <ypreethi56@gmail.com>
2024-10-07 07:59:17 -0700

    completed my footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <div>
+       <footer>
+   <div class="footer">
+    
+    <h2>swapskill</h2>
+    <p class="About">About</p>
+    <p>Contact us: <a href="mailto: someemail@gmail.com">abc@gmail.com</a></p>
+
+<p>@ 2024 Swap Skill. All rights reserved</p>
+   </div>
+</footer>
+    </div>
+  )
+}
+
+export default footer
