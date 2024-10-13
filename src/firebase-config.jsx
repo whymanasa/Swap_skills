5710cfcec3d2e647910cc5199f471cc14899ef12
manasa y <123yapitsme@gmail.com>
2024-10-13 19:11:46 +0530

    adding firebase authentication

---
+++ src/firebase-config.jsx
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIwLvE8HAOrewaFB0XyqQc7wPSx1lkpQ4",
+  authDomain: "swap-em-24152.firebaseapp.com",
+  projectId: "swap-em-24152",
+  storageBucket: "swap-em-24152.appspot.com",
+  messagingSenderId: "825127939835",
+  appId: "1:825127939835:web:e70b75747846e30fd90a5e",
+  measurementId: "G-8TSGEG81WM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
