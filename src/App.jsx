6c921bea24c0f08c11996a86d1a908debfe32632
Shre-yeah <hishreyadesh@gmail.com>
2024-11-03 20:25:59 +0530

    commit one

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import { BrowserRouter as Router, Routes, Route , Link} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+
 import About from './components/About';
+
 
 function App() {
   return (
     <Router>
       <div>
         <Navbar />
+        <Footer/>
         <Routes>
           <Route path="/" element={<Home />} />
            <Route  path="/About" element={<About />} />
         
         </Routes>
+        
       </div>
     </Router>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import img from "../assets/img-1.webp";
+import image from "../assets/img-2.jpeg";
 
+import "../styles/Home.css";
 function Home() {
   return (
-    <div>
-      home
+      <div className="container">
+        <div className="content-left">
+          <h1>A cool barter system</h1>
+          <p>Exchange your skills for a new one</p>
+          <button className="cta-button">Let's Start</button>
+        </div>
+        <div className="content-right">
+        
+        <img src={image} alt="hero image" className="image2"/>
+        <img src={img} alt="hero image" className="image1" />
+        <p>ʕ⁠⁠ꈍ⁠ᴥ⁠ꈍ⁠ʔ</p>
+        </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
